5ff5cfb2e370b71d6e6cc47d638efc8a55d978ae
oenu <51684443+oenu@users.noreply.github.com>
2026-02-16 17:46:34 -0500

    Added logging

--- server/src/main/java/dev/zroute/server/config/WebConfig.java
+++ server/src/main/java/dev/zroute/server/config/WebConfig.java
@@ -1,6 +1,8 @@
 package dev.zroute.server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,4 +20,17 @@ public class WebConfig implements WebMvcConfigurer {
         .allowCredentials(true)
         .maxAge(3600);
   }
+
+  @Bean
+  public CommonsRequestLoggingFilter requestLoggingFilter() {
+    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+    loggingFilter.setIncludeClientInfo(true);
+    loggingFilter.setIncludeQueryString(true);
+    loggingFilter.setIncludePayload(true);
+    loggingFilter.setMaxPayloadLength(10000);
+    loggingFilter.setIncludeHeaders(true);
+    loggingFilter.setAfterMessagePrefix("REQUEST DATA : ");
+    return loggingFilter;
+  }
 }
+
